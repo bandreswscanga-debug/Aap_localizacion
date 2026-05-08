0dd04bb038ff0de98147829030be6c55e8f5c774
angieccf469-jpg <1angiecarrillo12340300@gmail.com>
2026-05-08 12:56:57 -0500

    feat: agregar modelo Node con coordenadas y nodos iniciales

---
+++ lib/services/node.dart
@@ -0,0 +1,12 @@
+class Node {
+  final String name;
+  final double lat;
+  final double lng;
+
+  Node(this.name, this.lat, this.lng);
+}
+
+List<Node> nodes = [
+  Node("Nodo A", 4.705, -74.231),
+  Node("Nodo B", 4.710, -74.230),
+];
